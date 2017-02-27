a6b283265b3b6274f9f097fd1189c8d915cf2f1f
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-02-27 14:23:20 +0200

    Add Hello world GraphQL server

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log('Hello');
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+import * as express from 'express'
+import * as graphqlHTTP from 'express-graphql'
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
+});
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+app.listen(9002);
+
+console.log('http://localhost:9002/graphql');

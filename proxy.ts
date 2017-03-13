4ecfc7e13d153779fc47c53ffbaf151d177c0c96
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-03-13 11:06:02 +0200

    Add missing 'proxy.ts' file.

---
+++ proxy.ts
@@ -0,0 +1,30 @@
+import * as graphqlFetch from 'graphql-fetch';
+import {
+  parse,
+  extendSchema,
+  buildClientSchema,
+  introspectionQuery,
+} from 'graphql';
+
+import { fakeSchema } from './fake_schema';
+
+export function proxyMiddleware(url) {
+  const remoteServer = graphqlFetch(url) as
+    (query:String, vars?:any, opts?:any) => Promise<any>;
+
+  return remoteServer(introspectionQuery).then(introspection => {
+    //TODO: check for errors
+    const serverSchema = buildClientSchema(introspection.data);
+    return (extensionIDL, request, params) => {
+      const schema = extendSchema(serverSchema, parse(extensionIDL));
+      fakeSchema(schema);
+
+      //TODO fail if params.operationName set
+      //TODO copy headers
+      return remoteServer(params.query, params.variables).then(responce => {
+        //TODO proxy error
+        return { schema, rootValue: responce.data};
+      });
+    };
+  })
+}

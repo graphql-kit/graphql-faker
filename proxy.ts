4d90286788a9bdad8d1829c6b6883c457686c9c4
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-03-13 13:48:15 +0200

    Proxy: strip extended fields from query

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,11 @@
 import * as graphqlFetch from 'graphql-fetch';
 import {
   parse,
+  print,
+  visit,
+  TypeInfo,
   extendSchema,
+  visitWithTypeInfo,
   buildClientSchema,
   introspectionQuery,
 } from 'graphql';
@@ -16,19 +20,67 @@ export function proxyMiddleware(url) {
     //TODO: check for errors
     const serverSchema = buildClientSchema(introspection.data);
     return (extensionIDL, request, params) => {
-      const schema = extendSchema(serverSchema, parse(extensionIDL));
+      const extensionAST = parse(extensionIDL);
+      const extensionFields = getExtensionFields(extensionAST);
+      const schema = extendSchema(serverSchema, extensionAST);
       fakeSchema(schema);
 
       //TODO fail if params.operationName set
       //TODO copy headers
-      const query = params.query;
-      if (!query)
+      const originalQuery = params.query;
+      if (!originalQuery)
         return { schema };
 
+      const query = stripQuery(schema, originalQuery, extensionFields);
+      console.log(query);
+      //TODO: also cleanup params
       return remoteServer(query, params.variables).then(responce => {
         //TODO proxy error
         return { schema, rootValue: responce.data};
       });
     };
   })
+}
+
+function getExtensionFields(extensionAST) {
+  return {
+    Person: ['pet']
+  };
+}
+
+const typenameAST = {
+  kind: 'Field',
+  name: {
+    kind: 'Name',
+    value: '__typename',
+  },
+};
+
+function stripQuery(schema, query, extensionFields) {
+  const queryAST = parse(query);
+  const typeInfo = new TypeInfo(schema);
+
+  //TODO: inline all fragments
+  const changedAST = visit(queryAST, visitWithTypeInfo(typeInfo, {
+    Field() {
+      const typeName = typeInfo.getParentType().name;
+      const fieldName = typeInfo.getFieldDef().name;
+
+      if ((extensionFields[typeName] || []).includes(fieldName))
+        return null;
+    },
+    SelectionSet: {
+      leave(node) {
+        return {
+          kind: node.kind,
+          selections: [
+            ...node.selections,
+            typenameAST,
+          ],
+        };
+      }
+    },
+  }), null);
+
+  return print(changedAST);
 }

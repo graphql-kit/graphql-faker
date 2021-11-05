1159847a64671197a7d1a6206826588100a438f7
TheCodeAssassin <github@tca0.nl>
2021-11-05 21:20:50 +0100

    Fix deprecation warning
    Fixes: 
    
    ```
    Deprecation Warning: faker.random.uuid is now located in faker.datatype.uuid
    ```

--- src/fake.ts
+++ src/fake.ts
@@ -178,7 +178,7 @@ const fakeFunctions = {
     func: (min, max, precision) =>
       faker.datatype.number({ min, max, precision }),
   },
-  uuid: () => faker.random.uuid(),
+  uuid: () => faker.datatype.uuid(),
   word: () => faker.random.word(),
   words: () => faker.random.words(),
   locale: () => faker.random.locale(),
